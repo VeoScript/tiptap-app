db2eecc724571de354f408767b991eee2863ffdd
Jerome Villaruel <jerome.halcyondigital@gmail.com>
2023-08-04 11:06:09 +0800

    feat: update richtext layout and add zustand
    Co-Authored-By: Jerome Villaruel <107969452+veoscript-halcyon@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
 import Head from "next/head";
 import TipTap from "~/components/TipTap";
 
+import { documentStore } from "~/store";
+
 export default function Home() {
+  const { documentValue } = documentStore();
+
   return (
     <>
       <Head>
         <title>TipTap Richtext Editor</title>
       </Head>
       <div className="flex flex-grow items-center justify-center w-full h-full p-10 font-roboto-mono">
-        <div className="flex justify-center w-full max-w-3xl">
-          <TipTap />
+        <div className="flex flex-row items-start justify-center w-full space-x-3">
+          <div className="flex w-full max-w-3xl">
+            <TipTap />
+          </div>
+          <div className="flex flex-col w-full h-full min-h-[20rem] overflow-hidden rounded-xl border-2 border-black">
+            <h1 className="w-full p-3 font-bold text-base text-white bg-black">
+              Document Sample Results
+            </h1>
+            <article
+              className="prose w-full max-w-full p-3"
+              dangerouslySetInnerHTML={{ __html: documentValue }}
+            />
+          </div>
         </div>
       </div>
     </>
