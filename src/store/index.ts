4bea1bee822680402f4e77921ed4531f655ecc71
Jerome Villaruel <jerome.halcyondigital@gmail.com>
2023-08-04 12:19:59 +0800

    feat: add view create and delete story
    Co-Authored-By: Jerome Villaruel <107969452+veoscript-halcyon@users.noreply.github.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,34 @@
 import { create } from "zustand";
 
-export interface RichTextProps {
+export interface DocumentStoreProps {
   documentValue: string;
   setDocumentValue: (value: string) => void;
 }
 
-export const documentStore = create<RichTextProps>((set) => ({
+export interface StoryStoreProps {
+  stories: any[];
+  setStories: (data: { title: string; value: string }) => void;
+  deleteStory: (value: string) => void;
+}
+
+export const documentStore = create<DocumentStoreProps>((set) => ({
   documentValue: "",
   setDocumentValue: (value: string) => set({ documentValue: value }),
+}));
+
+export const storyStore = create<StoryStoreProps>((set, get) => ({
+  stories: [],
+  setStories: (data: { title: string; value: string }) =>
+    set({
+      stories: [...get().stories, data],
+    }),
+  deleteStory: (value: string) => {
+    const currentStories = get().stories;
+    const updatedStories = currentStories.filter(
+      (story: any) => story.title !== value
+    );
+    set({
+      stories: updatedStories,
+    });
+  },
 }));
